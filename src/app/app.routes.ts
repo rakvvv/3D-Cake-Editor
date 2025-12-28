1136bf83778f621fa96bcc5fca8129f9176a18ee
Rafał <rl125140@stud.ur.edu.pl>
2025-12-28 16:20:15 +0100

    Removed admin panel (#70)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,12 @@ import { LoginComponent } from './cake-editor/auth/login.component';
 import { RegisterComponent } from './cake-editor/auth/register.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { authGuard } from './services/auth.guard';
-import { AdminDashboardComponent } from './project-list/admin-dashboard.component';
-import { adminGuard } from './services/admin.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'projects', component: ProjectListComponent, canActivate: [authGuard] },
   { path: 'editor/:projectId', component: CakeEditorComponent, canActivate: [authGuard] },
-  { path: 'admin', component: AdminDashboardComponent, canActivate: [authGuard, adminGuard] },
   { path: '', pathMatch: 'full', redirectTo: 'projects' },
   { path: '**', redirectTo: 'projects' }
 ];
